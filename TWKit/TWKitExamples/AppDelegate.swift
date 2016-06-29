3362ab98698245c19b4fa2d5644e7de20828fa7e
許郁棋 <roy@Roys-MacBook-12.local>
2016-06-29 11:21:02 +0800

    add example root list

--- TWKit/TWKitExamples/AppDelegate.swift
+++ TWKit/TWKitExamples/AppDelegate.swift
@@ -9,38 +9,76 @@
 import UIKit
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder {
 
     var window: UIWindow?
-
-
-    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
-        return true
+    
+    enum Row: Int {
+        
+        case Plain
+        
+        var title: String {
+        
+            switch self {
+            case .Plain: return "Plain"
+            }
+        
+        }
+    
     }
-
-    func applicationWillResignActive(application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-    }
-
-    func applicationDidEnterBackground(application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(application: UIApplication) {
-        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
+    
+    let rows: [Row] = [ .Plain ]
+    
+    lazy var listTableViewController: TWTableViewController<TWTableViewCell> = { [unowned self] in
+    
+        let controller = TWTableViewController(cellType: TWTableViewCell.self)
+        
+        controller.navigationItem.title = "TWKit"
+        controller.cellHeight = .Fixed(height: 44.0)
+        controller.cellConfigurator = { cell, index in
+            
+            cell.textLabel?.text = self.rows[index].title
+            
+        }
+        controller.numberOfRows = self.rows.count
+        controller.tableView.delegate = self
+        
+        return controller
+        
+    }()
 
 }
 
+
+// MARK: - UIApplicationDelegate
+
+extension AppDelegate: UIApplicationDelegate {
+    
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        window?.rootViewController = UINavigationController(rootViewController: listTableViewController)
+        window?.makeKeyAndVisible()
+        
+        return true
+        
+    }
+    
+}
+
+
+// MARK: - UITableViewDelegate
+
+extension AppDelegate: UITableViewDelegate {
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+        let row = rows[indexPath.row]
+        
+        switch row {
+        case .Plain: print(row.title)
+        }
+        
+    }
+    
+}
