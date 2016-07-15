24cb6b318b5741cfba50e083c8b6be05fe3ceb05
Roy Hsu <roy@Roys-MacBook-12.local>
2016-07-15 13:15:15 +0800

    dynamic height for cache demo

--- Chocolate/ChocolateExamples/CacheIntegration/CacheIntegrationTableViewController.swift
+++ Chocolate/ChocolateExamples/CacheIntegration/CacheIntegrationTableViewController.swift
@@ -21,7 +21,11 @@ public class CacheIntegrationTableViewController: CHCacheTableViewController {
         
     }
     
-    required public init?(coder aDecoder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+    required public init?(coder aDecoder: NSCoder) {
+        
+        fatalError("init(coder:) has not been implemented")
+    
+    }
     
     
     // MARK: View Life Cycle
