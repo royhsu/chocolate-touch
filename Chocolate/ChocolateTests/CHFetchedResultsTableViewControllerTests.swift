ac1228c03e624a21717c67bc78a0e951e273b363
Roy Hsu <roy.hsu@tinyworld.cc>
2016-09-12 18:00:24 +0800

    add test for table view delegate

--- Chocolate/ChocolateTests/CHFetchedResultsTableViewControllerTests.swift
+++ Chocolate/ChocolateTests/CHFetchedResultsTableViewControllerTests.swift
@@ -18,7 +18,25 @@ class CHFetchedResultsTableViewControllerTests: XCTestCase {
     override func setUp() {
         super.setUp()
         
-        stack = try! CoreDataStack(name: "", model: NSManagedObjectModel(), options: nil, storeType: .memory)
+        let nameProperty = NSAttributeDescription()
+        nameProperty.name = "name"
+        nameProperty.attributeType = .stringAttributeType
+        nameProperty.isOptional = false
+        
+        let firstLetterOfName = NSAttributeDescription()
+        firstLetterOfName.name = "firstLetterOfName"
+        firstLetterOfName.attributeType = .stringAttributeType
+        firstLetterOfName.isOptional = false
+        
+        let entity = NSEntityDescription()
+        entity.name = "Person"
+        entity.properties.append(nameProperty)
+        entity.properties.append(firstLetterOfName)
+        
+        let model = NSManagedObjectModel()
+        model.entities.append(entity)
+
+        stack = try! CoreDataStack(name: "", model: model, options: nil, storeType: .memory)
         
     }
     
@@ -31,18 +49,42 @@ class CHFetchedResultsTableViewControllerTests: XCTestCase {
     
     
     func testInitWithFetchRequest() {
-
+        
+        let context = stack!.viewContext
+        
+        let roy = NSEntityDescription.insertNewObject(forEntityName: "Person", into: context)
+        roy.setValue("Roy", forKey: "name")
+        roy.setValue("r", forKey: "firstLetterOfName")
+        
+        let allen = NSEntityDescription.insertNewObject(forEntityName: "Person", into: context)
+        allen.setValue("Allen", forKey: "name")
+        allen.setValue("a", forKey: "firstLetterOfName")
+        
+        let alex = NSEntityDescription.insertNewObject(forEntityName: "Person", into: context)
+        alex.setValue("Alex", forKey: "name")
+        alex.setValue("a", forKey: "firstLetterOfName")
+        
+        try! context.save()
+        
         let fetchRequest = NSFetchRequest<NSManagedObject>()
-        fetchRequest.sortDescriptors = []
+        fetchRequest.sortDescriptors = [
+            NSSortDescriptor(key: "name", ascending: true)
+        ]
         
         let fetchedResultsController = NSFetchedResultsController(
             fetchRequest: fetchRequest,
             managedObjectContext: stack!.viewContext,
-            sectionNameKeyPath: nil,
+            sectionNameKeyPath: "firstLetterOfName",
             cacheName: nil
         )
         
         let fetchedResultsTableViewController = CHFetchedResultsTableViewController(fetchedResultsController: fetchedResultsController)
+        let tableView = fetchedResultsTableViewController.tableView!
+        tableView.isHidden = true
+        
+        let numberOfSections = tableView.numberOfSections
+        
+        XCTAssertEqual(numberOfSections, 2, "Number of sections doesn't match.")
         
     }
     
